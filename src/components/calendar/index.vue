f33208097237095cba6fd7fbd9f1ee2f1656f4ac
xiaojinfeng <xiaojinfeng@wesai.com>
2018-08-23 18:24:41 +0800

    日历完善

--- src/components/calendar/index.vue
+++ src/components/calendar/index.vue
@@ -1,29 +1,32 @@
 <template>
-	<div>
-		<div class="calendar-content-box">
-			<div class="calendar-top-content">
-				<span class="oper-arrow" @click="preMonth"><i class="iconfont icon-arrow-left"></i></span>
-				<span>{{formatDate}}</span>
-				<span class="oper-arrow" @click="nextMonth"><i class="iconfont icon-arrow-right"></i></span>
+	<div class="calendar-content-box" :class="classStr">
+		<div class="calendar-top-content">
+			<span class="oper-arrow" @click="preMonth"><i class="iconfont icon-arrow-left"></i></span>
+			<span>{{nowShowDate | format}}</span>
+			<span class="oper-arrow right-arrow" @click="nextMonth"><i class="iconfont icon-arrow-right"></i></span>
+			<span class="today-cls" @click="today"><i class="iconfont" :class="toDayClass"></i></span>
+		</div>
+		<div>
+			<div class="week-content">
+				<span v-for="(item, index) in weekArr">{{item}}</span>
 			</div>
-			<div>
-				<div class="week-content">
-					<span v-for="(item, index) in weekArr">{{item}}</span>
-				</div>
-				<div class="day-content">
-					<div v-for="(item, index) in daysArr" class="day-row">
-						<span v-for="(value, key) in item" @click="chooseDate(value,index,key)"
-							  :class="{isToday:value.isToday,notNowMonth:value.type !='now',nowMonth:value.type=='now',isChoose:value.isChoose}">{{value.num}}</span>
+			<div class="day-content">
+				<div v-for="(item, index) in daysArr" class="day-row">
+					<div v-for="(value, key) in item" class="">
+							<span @click="chooseDate(value,index,key)"
+								  :class="{isToday:value.isToday,notNowMonth:value.type !='now',nowMonth:value.type=='now',isChoose:value.isChoose}">{{value.num}}</span>
 					</div>
-
 				</div>
 
 			</div>
 
 		</div>
+
 	</div>
 </template>
 <style lang="scss" scoped>
+	@import "./font/iconfont.css";
+
 	.calendar-content-box {
 		width: 100%;
 		margin: 0 auto;
@@ -31,12 +34,36 @@
 		font-size: 16px;
 		padding: 10px;
 		box-sizing: border-box;
+
+		moz-user-select: -moz-none;
+		-moz-user-select: none;
+		-o-user-select: none;
+		-khtml-user-select: none;
+		-webkit-user-select: none;
+		-ms-user-select: none;
+		user-select: none;
+
 		.oper-arrow {
 			cursor: pointer;
+			float: left;
+			padding-left: 10px;
+		}
+		.right-arrow {
+			float: right;
+			padding-right: 10px;
+		}
+
+		.today-cls {
+			float: right;
+			cursor: pointer;
+			i {
+				font-size: 20px;
+			}
 		}
 		.week-content, .day-row {
 			display: flex;
-			justify-content: space-between
+			justify-content: space-between;
+			margin-top: 5px;
 		}
 		.week-content, .day-content {
 			line-height: 40px;
@@ -68,6 +95,21 @@
 <script>
 	import Vue from 'vue';
 
+	Vue.filter('format', function (data) {
+		var date = new Date(data);
+		var y = date.getFullYear(),
+			m = date.getMonth() + 1,
+			d = date.getDate(),
+			h = date.getHours(),
+			mm = date.getMinutes(),
+			ss = date.getSeconds();
+		if (m < 10) m = '0' + m;
+		if (d < 10) d = '0' + d;
+		if (h < 10) h = '0' + h;
+		if (mm < 10) mm = '0' + mm;
+		if (ss < 10) ss = '0' + ss;
+		return y + '年' + m + "月";
+	});
 	export default {
 		data() {
 			return {
@@ -78,28 +120,44 @@
 				weekArr: ["日", "一", "二", "三", "四", "五", "六"]
 			}
 		},
-
+		computed: {
+			toDayClass: function () {
+				var date = new Date();
+				var y = date.getFullYear(),
+					m = date.getMonth() + 1,
+					d = date.getDate(),
+					h = date.getHours(),
+					mm = date.getMinutes(),
+					ss = date.getSeconds();
+				if (m < 10) m = '0' + m;
+				if (d < 10) d = '0' + d;
+				if (h < 10) h = '0' + h;
+				if (mm < 10) mm = '0' + mm;
+				if (ss < 10) ss = '0' + ss;
+				return "icon-rili" + d;
+			}
+		},
 		props: {
-			nowDate: {
+			defaultDate: {
 				type: Date,
 				default: function () {
 					return new Date();
 				}
+			},
+			classStr: {
+				type: String,
+				default: ""
 			}
 		},
 
 		mounted() {
-			if (this.nowDate) {
-				this.nowShowDate = this.nowDate;
-				// this.year = new Date(this.nowShowDate).getFullYear();
-				// this.month = new Date(this.nowShowDate).getMonth() + 1;
+			if (this.defaultDate) {
+				this.nowShowDate = this.defaultDate;
 			}
-			this.formatDate = this.formatYearMonth(new Date(this.nowShowDate).getTime());
-			this.getDateList();
+			// this.getDateList();
 		},
 		methods: {
 			getDateList() {
-				this.daysArr = [];
 				let year = new Date(this.nowShowDate).getFullYear(), month = new Date(this.nowShowDate).getMonth() + 1;
 				let days = (new Date(year, month, 0)).getDate();
 				let preDays = (new Date(year, month - 1, 0)).getDate();
@@ -114,7 +172,7 @@
 						"type": "now",
 						"isToday": false,
 						"date": year + "/" + month + "/" + i,
-						"isChoose": false
+						"isChoose": this.chooseObj.date == (year + "/" + month + "/" + i) ? true : false
 					};
 					if (new Date(year, month - 1, i).getTime() == (new Date(this.formatYearMonthDay(new Date().getTime()))).getTime()) {
 						datObj.isToday = true;
@@ -133,7 +191,7 @@
 						"week": weekNum,
 						"type": "pre",
 						"isToday": false,
-						"date": year + "/" + (month - 1) + "/" + (preDays - i),
+						"date": ((month - 1) < 1 ? year - 1 : year) + "/" + ((month - 1) < 1 ? -((month - 1) - 12) : (month - 1)) + "/" + (preDays - i),
 						"isChoose": false
 					}
 					if (new Date(year, month - 1, i).getTime() == (new Date(this.formatYearMonthDay(new Date().getTime()))).getTime()) {
@@ -150,7 +208,7 @@
 						"week": weekNum,
 						"type": "next",
 						"isToday": false,
-						"date": year + "/" + (month + 1) + "/" + i,
+						"date": (month + 1 > 12 ? year + 1 : year) + "/" + ((month + 1) > 12 ? (month + 1) - 12 : (month + 1)) + "/" + i,
 						"isChoose": false
 					}
 					if (new Date(year, month - 1, i).getTime() == (new Date(this.formatYearMonthDay(new Date().getTime()))).getTime()) {
@@ -159,22 +217,6 @@
 					daysArr[daysArr.length - 1].push(datObj);
 				}
 				this.daysArr = daysArr;
-				this.chooseFnc();
-			},
-			formatYearMonth(timestamp) {
-				var date = new Date(parseInt(timestamp));
-				var y = date.getFullYear(),
-					m = date.getMonth() + 1,
-					d = date.getDate(),
-					h = date.getHours(),
-					mm = date.getMinutes(),
-					ss = date.getSeconds();
-				if (m < 10) m = '0' + m;
-				if (d < 10) d = '0' + d;
-				if (h < 10) h = '0' + h;
-				if (mm < 10) mm = '0' + mm;
-				if (ss < 10) ss = '0' + ss;
-				return y + '-' + m;
 			},
 			formatYearMonthDay(timestamp) {
 				var date = new Date(parseInt(timestamp));
@@ -195,13 +237,12 @@
 				let year = item.date.split("/")[0],
 					month = item.date.split("/")[1];
 				this.chooseObj = item;
+				this.$emit('chooseDay', item.date);
 				if (item.type !== "now") {
 					this.nowShowDate = new Date(year, month, 0);
 					return false;
 				}
-
 				this.chooseFnc();
-
 			},
 			chooseFnc() {
 				if (this.chooseObj && this.chooseObj.date) {
@@ -214,8 +255,6 @@
 							}
 						})
 					})
-
-					console.log(this.daysArr)
 				}
 			},
 			preMonth() {
@@ -227,12 +266,14 @@
 				let year = this.nowShowDate.getFullYear(),
 					month = this.nowShowDate.getMonth() + 1;
 				this.nowShowDate = new Date(year, month + 1, 0);
+			},
+			today() {
+				this.nowShowDate = new Date();
 			}
 		},
 		watch: {
 			nowShowDate: function (nowValue, oldValue) {
 				this.getDateList();
-				this.formatDate = this.formatYearMonth(new Date(nowValue).getTime());
 			}
 		}
 
--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,12 +1,19 @@
 import Vue from 'vue';
 import Calendar from "@/components/calendar"
+
 export default {
 	data() {
 		return {
 			msg: '',
+			defaultDate: new Date("2018/07/07")
 		}
 	},
 	components: {
 		Calendar,
 	},
+	methods: {
+		chooseDay(date) {
+			console.log("date", date)
+		}
+	}
 }
